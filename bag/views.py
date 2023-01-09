2dc62cff1b241d23e6c05d0d02582efba90fe829
Angharad Caswell <ange.caswell1@gmail.com>
2023-01-09 12:20:54 +0000

    added views to update items in bags

--- bag/views.py
+++ bag/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import (
+    render, redirect, reverse, HttpResponse, get_object_or_404
+)
+from django.contrib import messages
+from products.models import Product
 
 
 def view_bag(request):
     """ A view that renders the bag contents page """
 
     return render(request, 'bag/bag.html')
+
 
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
@@ -20,3 +25,25 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    product = get_object_or_404(Product, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    bag = request.session.get('bag', {})
+
+    if quantity > 0:
+        bag[item_id] = quantity
+        messages.success(request,
+                            (f'Updated {product.name} '
+                            f'quantity to {bag[item_id]}'))
+    else:
+        bag.pop(item_id)
+        messages.success(request,
+                            (f'Removed {product.name} '
+                            f'from your bag'))
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
