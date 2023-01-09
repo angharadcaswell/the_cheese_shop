e7942560c05e72b96bd6bfbe8cb64f5cd0e850af
Angharad Caswell <ange.caswell1@gmail.com>
2023-01-09 18:19:05 +0000

    Added missing checkout folder for css and js. Added start of Stripe functionality

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51MOPMQEElqczpmTtWlIn8couJrucw4nQsQA4LZXSm8T9TPvjiQY0FylEh2dNHEUMlNyW9URWVGh1GTKSPkbWw7Q100LoydwWRd',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
