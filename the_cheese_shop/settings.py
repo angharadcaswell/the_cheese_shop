b03e5455c6ee41deb7c9108cec54c9ab544e8fa4
Angharad Caswell <ange.caswell1@gmail.com>
2023-01-10 09:46:42 +0000

    added secret key

--- the_cheese_shop/settings.py
+++ the_cheese_shop/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rw8n0gds*blb_7f+k75hdn1rk$q*y02dasov0rpy^y=20%hqyc'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['the-cheese-shop-1.herokuapp.com', 'localhost']
 
